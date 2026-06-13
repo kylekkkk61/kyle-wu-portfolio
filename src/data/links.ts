2d649931f01b097c475b39fe5abd7d5da5e64f09
kylewu <102206550+kylekkkk61@users.noreply.github.com>
2026-06-13 14:24:16 +0800

    feat: add social icons to hero section (#28)

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,6 +1,7 @@
 export const links = {
   linkedin: "https://www.linkedin.com/in/kylewu0610/",
   email: "mailto:itskyle610610@gmail.com",
+  telegram: "https://t.me/kylekkkkwu",
   github: "https://github.com/kylekkkk61",
   resume: "/resume",
   resumePdf: "https://cv.kylekkkk.com/kyle-wu-resume.pdf",
